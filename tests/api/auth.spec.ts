f9acf5cddb37c20a334e8eb832b1f9e82d9b3809
NupurParalkar <nupur.paralkar@gmail.com>
2026-03-14 00:00:55 +0530

    npx playwright test --grep @smoke

--- tests/api/auth.spec.ts
+++ tests/api/auth.spec.ts
@@ -10,3 +10,8 @@ test('@smoke @api API user list should return valid response', async ({ request 
    expect(responseBody.length).toBeGreaterThan(0);
   expect(responseBody[0]).toHaveProperty('email');
 });
+test('@smoke @api Invalid endpoint should return 404', async ({ request }) => {
+  const response = await request.get('https://jsonplaceholder.typicode.com/invalid-url');
+
+  expect(response.status()).toBe(404);
+});
