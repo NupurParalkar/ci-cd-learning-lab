8b37f1d459171ab10c8ce3190c3145db867947bc
NupurParalkar <nupur.paralkar@gmail.com>
2026-03-13 23:34:46 +0530

    Running API test using jsonplaceholder api

---
+++ tests/api/auth.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('@smoke @api API user list should return valid response', async ({ request }) => {
+  const response = await request.get('https://jsonplaceholder.typicode.com/users');
+
+  expect(response.status()).toBe(200);
+
+  const responseBody = await response.json();
+
+   expect(responseBody.length).toBeGreaterThan(0);
+  expect(responseBody[0]).toHaveProperty('email');
+});
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,8 +2,7 @@ import { test, expect } from '@playwright/test';
 import path from 'path';
 import { LoginPage } from '../pages/LoginPage';
 
-test('Invalid login should show error', { tag: '@smoke',   }, async ({ page }) => {
-
+test('@smoke @ui Invalid login should show error', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   const filePath = `file://${path.resolve('demo-app/login.html')}`;
